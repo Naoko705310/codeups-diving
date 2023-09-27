6d65664575371610e86102e104c70096398527a0
Naoko705310 <naokos79@gmail.com>
2023-09-27 20:19:01 +0700

    information アニメーション

--- src/js/script.js
+++ src/js/script.js
@@ -33,6 +33,7 @@ jQuery(function ($) {
     }, 1000); // スクロールのアニメーション時間を調整
   });
 
+
   // ヘッダーの色を変える（FadeInすなわちdisplay:blockにする）
   // $('.js-header').on('click', function () {
   //   if ($('.js-header').hasClass('is-open')) {
@@ -74,6 +75,34 @@ jQuery(function ($) {
       }
     },
   });
+
+
+  // ボタンのアニメーション
+//要素の取得とスピードの設定
+var box = $('.colorbox'),
+    speed = 700;  
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+  $(this).append('<div class="color"></div>')
+  var color = $(this).find($('.color')),
+  image = $(this).find('img');
+  var counter = 0;
+
+  image.css('opacity','0');
+  color.css('width','0%');
+  //inviewを使って背景色が画面に現れたら処理をする
+  color.on('inview', function(){
+    if(counter == 0){
+    $(this).delay(200).animate({'width':'100%'},speed,function(){
+        image.css('opacity','1');
+        $(this).css({'left':'0' , 'right':'auto'});
+        $(this).animate({'width':'0%'},speed);
+    })
+        counter = 1;
+      }
+    });
+});//画像エフェクト閉じタグ
 
 
 }); //jQuery 閉じタグ
