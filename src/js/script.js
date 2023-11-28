6375818c3fcb0e1d09a035ffca9d0e202531cecc
Naoko705310 <naokos79@gmail.com>
2023-11-28 10:03:46 +0700

    パンくず矢印修正済み

--- src/js/script.js
+++ src/js/script.js
@@ -156,6 +156,24 @@ jQuery(function ($) {
       event.stopPropagation();
     });
   });
+  // モーダルここまで
+
+  // page-information タブ
+  $(function () {
+    $('.js-tab-trigger').on('click', function () {
+        //まずは全triggerからclass削除
+        $('.js-tab-trigger').removeClass('is-active');
+        //次に全targetからclass削除
+        $('.js-tab-target').removeClass('is-active');
+        //次にクリックした要素にis-active
+        $(this).addClass('is-active');
+        //data属性を取得する
+        let id = $(this).data("id");
+        //data属性値=idが等しいものにclass付与
+        $('#' + id).addClass('is-active')
+    });
+  });
+  // タブここまで
 
   //FAQ下層ページ（アコーディオン）
   $(document).ready(function () {
