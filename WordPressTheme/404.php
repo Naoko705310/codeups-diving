6825be0ef3e5ae92ad144bd0d7ca3b859ac2b537
Naoko705310 <naokos79@gmail.com>
2024-07-18 10:38:17 +0700

    404エスケープ処理完了

--- WordPressTheme/404.php
+++ WordPressTheme/404.php
@@ -1,27 +1,46 @@
-<!-- 404 ページ -->
+<!-- privacy policy プライバシーポリシーとterms 利用規約 共通 -->
 <?php get_header(); ?>
+
+<?php
+// ページのタイトルを取得
+$page_title = get_the_title();
+
+// ※管理画面のタイトルは日本語（パンくずの表示は日本語にしたいから）で、この２ページのFVのみ英語にするため。
+// プライバシーポリシーまたは利用規約のページに対する条件分岐
+if (is_page('privacypolicy')) {
+  $page_title = 'Privacy Policy'; // 英語タイトルに上書き
+} elseif (is_page('terms-of-service')) {
+  $page_title = 'Terms of Service'; // 英語タイトルに上書き
+}
+?>
+
 <main>
-      <!-- 下層 コンテンツ -->
-      <section class="page-error-404 page-layout--404">
-      <!-- パンくずリスト -->
-      <?php get_template_part('parts/breadcrumb'); ?>
-      
-        <div class="page-error-404__inner inner">
-          <div class="page-error-404__contents-wrapper">
-            <h2 class="page-error-404__heading">
-              404
-            </h2>
-            <p class="page-error-404__text">
-              申し訳ありません。<br>
-              お探しのページが見つかりません。
-            </p>
-            <!-- ボタン -->
-            <div class="page-error-404__button">
-              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button button--white">
-                <span>page TOP</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+  <!-- 下層 FV -->
+  <section class="sub-fv">
+    <div class="sub-fv__inner">
+    <h2 class="sub-fv__heading">
+      <?php echo esc_html($page_title); ?>
+    </h2>
+      <picture class="sub-fv__image">
+        <source srcset="<?php echo esc_url(get_theme_file_uri('/assets/images/common/sub-privacy__fv-pc.jpg')); ?>" media="(min-width: 768px)">
+        <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/common/sub-privacy__fv-sp.jpg')); ?>" alt="<?php echo esc_attr('海中のサンゴの画像'); ?>">
+      </picture>
+    </div>
+  </section>
+  <!-- パンくずリスト -->
+  <?php get_template_part('parts/breadcrumb'); ?>
+  <!-- 下層 コンテンツ -->
+  <div class="page-privacy page-layout">
+    <div class="page-privacy__inner inner">
+      <!-- テキストは管理画面で編集 -->
+      <?php
+      if ( have_posts() ) : 
+          while ( have_posts() ) : the_post();
+              the_content();
+          endwhile;
+      endif;
+      ?>
+    </div>
+  </div>
+
 <?php get_footer(); ?>
